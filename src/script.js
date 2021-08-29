6e61311272fc59e5d6af395cf567b8748e491b8e
Sara Rizo <sara01rizo@gmail.com>
2021-08-29 19:43:32 +0200

    Added the principal id in HTML

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-alert(hello);
+
+
+let nameList = "https://api.agify.io?name[]=michael&name[]=matthew&name[]=jane";
+
+
+let firstName = prompt("Your name is");
+firstName = firstName.toLowerCase();
+if ([firstName] !== undefined) {
+    let currentAge = [firstName];
+}
