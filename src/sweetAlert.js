aabd400a14cdd512354f2a3fc68a053dc18a44d5
Sara Rizo <sara01rizo@gmail.com>
2021-08-29 20:42:02 +0200

    Updated file sweerAlert footer and confirm button

--- src/sweetAlert.js
+++ src/sweetAlert.js
@@ -1,5 +1,8 @@
 Swal.fire({
     title: 'Welcome!',
     text: 'Test The Incredible Predictorium',
-    html: '<b class="red">Hello!</b>',
+    html: '<b class="red">to The Incredible Predictorium 🔷 </b>',
+    icon: 'success',
+    confirmButtonText: 'Select',
+    footer: '100 % made with 💗',
 });
