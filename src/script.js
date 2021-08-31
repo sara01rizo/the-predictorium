b2250e705cf702a39b40c2837edb4aeba57a4276
Sara Rizo <sara01rizo@gmail.com>
2021-08-31 22:47:52 +0200

    Update HTML and JS  file - function display answer

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
+function displayAnswer() {
+  let answerAge = document.querySelector("answerAge");
 
+  let apiName = `https://api.agify.io?name[]=michael&name[]=matthew&name[]=jane`;
 
-let apiName = "https://api.agify.io?name[]=michael&name[]=matthew&name[]=jane";
+  console.log(apiName);
+}
 
+displayAnswer();
