185c3d45e87020350517976765e14d8131a4a11b
Sara Rizo <sara01rizo@gmail.com>
2021-09-08 23:52:27 +0200

    Updated JS file - take off apiName

--- src/script.js
+++ src/script.js
@@ -1,14 +1,6 @@
 let askNname = [];
 
 function displayAnswer() {
-  let answerAge = document.querySelector("answerAge");
-
-  let apiName = `https://api.agify.io?name[]=michael&name[]=matthew&name[]=jane`;
-
-  console.log(apiName);
-}
-
-function displayAnswer() {
   let agePredicted = document.getElementById("demo");
   agePredicted.innerHTML = Math.floor(Math.random() * 100 + 1);
 }
