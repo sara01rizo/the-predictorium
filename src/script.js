8bb995692564d5267ed1572e41f32ab9b05830d4
Sara Rizo <sara01rizo@gmail.com>
2021-09-09 00:01:54 +0200

    Updated JS and html File added ID age

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ let askNname = [];
 function displayAnswer() {
   let agePredicted = document.getElementById("demo");
   agePredicted.innerHTML = Math.floor(Math.random() * 100 + 1);
+
+  let ageElement = document.querySelector("#age-now");
 }
 
 displayAnswer();
